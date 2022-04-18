2c03ddf3d3dd873d0dc224b0a4eac45650e97072
diego.mesa <diego.mesa@geniussports.com>
2022-04-18 18:27:16 -0500

    removing unused objects

--- src/main/kotlin/com/example/demodmesa/DemoDmesaApplication.kt
+++ src/main/kotlin/com/example/demodmesa/DemoDmesaApplication.kt
@@ -12,23 +12,6 @@ data class Greeting(var id: Long, var content:String)
 @SpringBootApplication
 class DemoDmesaApplication
 
-@RestController
-class GreetingsController {
-	val template = "Hello %s!"
-	val atomicLong = AtomicLong()
-
-	@GetMapping("/greeting")
-	fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String?): Greeting {
-		return Greeting(atomicLong.getAndIncrement(), String.format(template, name))
-	}
-
-	// I can directly put controllers on the HelloSpringApplication but rather not.
-	@GetMapping("/hello")
-	fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String? {
-		return String.format("Hello %s! Fox News - Foolandia", name)
-	}
-}
-
 fun main(args: Array<String>) {
 	runApplication<DemoDmesaApplication>(*args)
 }
